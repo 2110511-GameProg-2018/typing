3d8e8fc33d6c9da99619552e625330eaf38d2308
newplyz <art_gif_52@hotmail.com>
2018-04-02 15:57:33 +0700

    WordSet
    WordSet Loader,Wordset Generator

---
+++ Assets/Scripts/WordSet Loader.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class WordSetLoader : MonoBehaviour {
+    
+    public List<string> LoadWord(string filename) // "filename.txt"
+    {
+        List<string> set = new List<string> ();
+        string[] lines = File.ReadAllLines("./Assets/"+filename);
+        char[] delimiter = {' '};
+        foreach(string line in lines)
+        {
+            string[] words = line.Split(delimiter);
+            foreach(string word in words)
+            {
+                set.Add(word);
+            }
+        }
+        return set;
+    }
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/WordSet.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WordSet : MonoBehaviour {
+    List<string> Words = new List<string>();
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public List<string> getWords()
+    {
+        return Words;
+    }
+    public List<string> merge(WordSet a,WordSet b)
+    {
+        List<string> set1 = a.getWords();
+        List<string> set2 = b.getWords();
+        set1.AddRange(set2);
+        return set1;
+    }
+}
