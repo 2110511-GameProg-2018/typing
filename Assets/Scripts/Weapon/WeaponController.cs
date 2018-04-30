f265e2112b9d10087c0bbcab7df8f003e2efd1f3
Krittakan <ragunakan@gmail.com>
2018-04-30 13:49:09 +0700

    Unlock Weapon

--- Assets/Scripts/Weapon/WeaponController.cs
+++ Assets/Scripts/Weapon/WeaponController.cs
@@ -27,11 +27,11 @@ public class WeaponController : MonoBehaviour {
 		currentWeapon = 0;
 		player.setWeapon(weapons[currentWeapon].GetComponent<Weapon>());
 
-		// Unlock first weapon and lock the rest
-		UnlockWeapon(0);
-		for (int i = 1; i < weapons.Length; i++) {
-            if(!isWeaponUnlocked[i])
-			    LockWeapon(i);
+		for (int i = 0; i < weapons.Length; i++) {
+            if (!isWeaponUnlocked[i])
+                LockWeapon(i);
+            else
+                UnlockWeapon(i);
 		}
 	}
 	
